9bd9caf4d0d9ad1a5ad47ce2d338465f95fbfa35
pynaruer <62301358+pynaruer@users.noreply.github.com>
2020-05-20 10:09:45 +0200

    Initial commit

---
+++ Partie1.py
@@ -0,0 +1,45 @@
+from PySide2.QtWidgets import *
+
+class SQLClientWindow(QWidget):
+
+    def __init__(self):
+        QWidget.__init__(self)
+
+        self.setWindowTitle("SQL Client")
+        self.setMinimumSize(600, 400)
+        layout = QVBoxLayout()
+
+        buttonspanel = ButtonsPanel()
+        notificationPanel = QTextEdit()
+
+        resultTable = QTableWidget(5, 3)
+        resultTable.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+        layout.addWidget(buttonspanel)
+        layout.addWidget(notificationPanel)
+        layout.addWidget(resultTable)
+
+        self.setLayout(layout)
+
+class ButtonsPanel(QWidget):
+
+    def __init__(self):
+        QWidget.__init__(self)
+
+        layout = QHBoxLayout()
+
+        button_1 = QPushButton("Configure")
+        button_2 = QPushButton("Connect")
+        button_3 = QPushButton("Disconnect")
+
+        layout.addWidget(button_1)
+        layout.addWidget(button_2)
+        layout.addWidget(button_3)
+
+        self.setLayout(layout)
+
+if __name__ == "__main__":
+    app = QApplication([])
+    win1 = SQLClientWindow()
+    win1.show()
+    app.exec_()
---
+++ Partie2.py
@@ -0,0 +1,45 @@
+from PySide2.QtWidgets import *
+
+
+class LabeledTextField(QWidget):
+
+    def __init__(self, text):
+        QWidget.__init__(self)
+        self.text = text
+
+        layout = QHBoxLayout()
+
+        label = QLabel(text)
+        text_1 = QTextEdit()
+        text_1.setMaximumHeight(20)
+
+        layout.addWidget(label)
+        layout.addWidget(text_1)
+
+        self.setLayout(layout)
+
+class ConfigurationDialog(QDialog):
+
+    def __init__(self):
+        QDialog.__init__(self)
+
+        self.setWindowTitle("Configuration")
+        self.setMinimumSize(300, 100)
+
+        layout = QVBoxLayout()
+
+        label_1 = LabeledTextField("IP address")
+        label_2 = LabeledTextField("User")
+        label_3 = LabeledTextField("Password")
+
+        layout.addWidget(label_1)
+        layout.addWidget(label_2)
+        layout.addWidget(label_3)
+
+        self.setLayout(layout)
+
+if __name__ == "__main__":
+    app = QApplication([])
+    win = ConfigurationDialog()
+    win.show()
+    app.exec_()
